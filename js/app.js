5d210e805cd50d03c4d535300e9b804d467d4b96
shourab <shourab.cit.bd@gmail.com>
2022-10-09 19:04:35 +0600

    Banner done

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(function () {
   const searchArea = $(".searchArea");
   const searchLg = $(".search-lg");
   const searchSm = $(".search-sm");
-  
+
   searchLg.on("click", searchAreaToggler);
   searchSm.on("click", searchAreaToggler);
 
@@ -16,4 +16,28 @@ $(function () {
   }
 
   //*SEARCH BAR ENDS HERE
+
+  //*BANNER STARTS HERE
+  const productDetailSlider = $(".productDetailSlider");
+  const productMainImageSlider = $(".productMainImageSlider");
+  const thumbSlides = $(".thumbSlider .thubSlide");
+
+  productDetailSlider.slick({
+    arrows: false,
+    dots: false,
+    asNavFor: ".productMainImageSlider",
+    infinite: false,
+  });
+  productMainImageSlider.slick({
+    arrows: false,
+    dots: false,
+    infinite: false,
+    asNavFor: ".productDetailSlider",
+  });
+
+  thumbSlides.click(function () {
+    let key = $(this).data("key");
+    productMainImageSlider.slick("slickGoTo", key);
+  });
+  //*BANNER ENDS HERE
 });
